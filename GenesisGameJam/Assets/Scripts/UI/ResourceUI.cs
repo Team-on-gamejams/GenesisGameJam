e912f4a834fb61010c740f507a134554aabc4f40
Team <timonsol13@gmail.com>
2020-12-12 18:07:48 +0200

    Resources fly

--- GenesisGameJam/Assets/Scripts/UI/ResourceUI.cs
+++ GenesisGameJam/Assets/Scripts/UI/ResourceUI.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using Random = UnityEngine.Random;
 
 public class ResourceUI : MonoBehaviour {
 	[Header("Data"), Space]
 	[SerializeField] ResourceType type;
 
+	[Header("Audio"), Space]
+	[SerializeField] AudioClip counterSound;
+	AudioSource counteras;
+
 	[Header("Refs"), Space]
 	[SerializeField] TextMeshProUGUI textField;
+	[SerializeField] Image image;
 
 	Coroutine coroutine;
 	int oldValue;
@@ -21,6 +28,77 @@ public class ResourceUI : MonoBehaviour {
 
 	private void OnDestroy() {
 		GameManager.Instance.player.onResourceChange -= OnValueUpdated;
+	}
+
+	public void DropWithFlyingParticles(int delta, Vector3 worldPos) {
+		int pieaces = delta / 10 + (delta % 10 != 0 ? 1 : 0);
+		while (pieaces != 0) {
+			--pieaces;
+
+			GameObject meatgo = new GameObject("Flying resource");
+			meatgo.transform.SetParent(image.canvas.transform);
+			Image img = meatgo.AddComponent<Image>();
+			img.sprite = image.sprite;
+			Color c = img.color;
+			c.a = 0.0f;
+			img.color = c;
+			img.SetNativeSize();
+
+			meatgo.transform.position = TemplateGameManager.Instance.Camera.WorldToScreenPoint(worldPos + (Vector3)Random.insideUnitCircle);
+			meatgo.transform.localEulerAngles = new Vector3(0, 0, Random.Range(0, 360f));
+
+			LeanTween.value(0, 1, 0.5f)
+				.setOnUpdate((float a) => {
+					c = img.color;
+					c.a = a;
+					img.color = c;
+				})
+				.setOnComplete(() => {
+					Vector3 startPos = meatgo.transform.position;
+					Vector3 startAngle = meatgo.transform.localEulerAngles;
+					float dist = (image.transform.position - startPos).magnitude;
+
+					LeanTween.value(0, 1, dist / Screen.height * 0.8f)
+					.setDelay(0.1f * pieaces)
+					.setOnUpdate((float t) => {
+						meatgo.transform.position = Vector3.Lerp(startPos, image.transform.position, t);
+						meatgo.transform.localEulerAngles = Vector3.Lerp(startAngle, Vector3.zero, t);
+					});
+
+					bool isLastLoop = pieaces == 0;
+					LeanTween.value(1, 0, 0.2f)
+					.setDelay(0.1f * pieaces + dist / Screen.height * 0.64f)
+					.setOnStart(() => {
+						if (counteras == null) {
+							counteras = AudioManager.Instance.Play(counterSound, channel: AudioManager.AudioChannel.Sound);
+						}
+					})
+					.setOnUpdate((float t) => {
+						c = img.color;
+						c.a = t;
+						img.color = c;
+						meatgo.transform.localScale = Vector3.one * t;
+					})
+					.setEase(LeanTweenType.easeInCubic)
+					.setOnComplete(() => {
+						if (delta >= 10) {
+							GameManager.Instance.player[type] += 10;
+							delta -= 10;
+						}
+						else {
+							GameManager.Instance.player[type] += delta % 10;
+							delta = 0;
+						}
+
+						if (counteras != null && isLastLoop) {
+							counteras.Stop();
+							counteras = null;
+						}
+
+						Destroy(meatgo, Random.Range(0.1f, 1.0f));
+					});
+				});
+		}
 	}
 
 	void OnValueUpdated(ResourceType type, int newValue) {
@@ -41,19 +119,19 @@ public class ResourceUI : MonoBehaviour {
 			int fastAddValue = 10 + UnityEngine.Random.Range(0, 6);
 
 			if(difference > 0) {
-				if (absDifference > fastAddValue)
-					oldValue += fastAddValue;
-				else if (absDifference > 10)
-					oldValue += 10;
-				else
+				//if (absDifference > fastAddValue)
+				//	oldValue += fastAddValue;
+				//else if (absDifference > 10)
+				//	oldValue += 10;
+				//else
 					++oldValue;
 			}
 			else {
-				if (absDifference > fastAddValue)
-					oldValue -= fastAddValue;
-				else if (absDifference > 10)
-					oldValue -= 10;
-				else
+				//if (absDifference > fastAddValue)
+				//	oldValue -= fastAddValue;
+				//else if (absDifference > 10)
+				//	oldValue -= 10;
+				//else
 					--oldValue;
 			}
 
