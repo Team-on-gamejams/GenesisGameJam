88cd9aad7977e60a4bc1f1bd721cad42bdcd4127
Team <timonsol13@gmail.com>
2020-12-12 23:50:15 +0200

    Spawning units

---
+++ GenesisGameJam/Assets/Scripts/Building/MainTreeButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTreeButtons : MonoBehaviour {
+	[Header("Prefabs"), Space]
+	[SerializeField] GameObject workerWispPrefab;
+	[SerializeField] GameObject defenedWispPrefab;
+
+	[Header("Points"), Space]
+	[SerializeField] Transform centerPoint;
+	[SerializeField] Transform workerSpawnPoint;
+	[SerializeField] Transform defenderSpawnPoint;
+
+	public void SpawnWorkerWisp() {
+		GameObject go = Instantiate(workerWispPrefab, workerSpawnPoint.position, Quaternion.identity, null);
+	}
+
+	public void SpawnDefenderWisp() {
+		GameObject go = Instantiate(defenedWispPrefab, defenderSpawnPoint.position, Quaternion.identity, null);
+
+	}
+
+	public void Upgrade() {
+		Debug.LogWarning("Upgrade. [Not realised]");
+	}
+}
