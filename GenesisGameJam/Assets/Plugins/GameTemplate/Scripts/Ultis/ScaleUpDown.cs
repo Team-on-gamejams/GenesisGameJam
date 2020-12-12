381176a2cb3b6313431504249364b1afe156938c
Team <timonsol13@gmail.com>
2020-12-12 21:47:24 +0200

    Defender wisp

--- GenesisGameJam/Assets/Plugins/GameTemplate/Scripts/Ultis/ScaleUpDown.cs
+++ GenesisGameJam/Assets/Plugins/GameTemplate/Scripts/Ultis/ScaleUpDown.cs
@@ -30,15 +30,14 @@ public class ScaleUpDown : MonoBehaviour {
 
 		minL = 0.4f;//minScale - (maxScale - 1);
 		maxL = 1.0f;//minScale;
+
+		currTime = Random.Range(0, maxTime);
+		isIncreaseScale = Random.Range(0, 2) == 1;
 	}
 
 	void OnEnable() {
-		if (light != null) {
+		if (light == null) {
 			currTime = Mathf.InverseLerp(minScale, maxScale, gameObject.transform.localScale.x);
-		}
-		else {
-			currTime = Random.Range(0, currTime);
-			isIncreaseScale = Random.Range(0, 2) == 1;
 		}
 
 		gameObject.transform.localScale = Vector3.one * Mathf.SmoothStep(minScale, maxScale, currTime / maxTime);
